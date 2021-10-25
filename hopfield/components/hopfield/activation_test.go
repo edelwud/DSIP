45c3e459f1a75508404096275ec9c85f74723f13
edelwud <edelwud@icloud.com>
2021-10-25 14:52:34 +0300

    refactor: `activation`

--- hopfield/components/hopfield/activation_test.go
+++ hopfield/components/hopfield/activation_test.go
@@ -1,10 +1,6 @@
 package hopfield
 
 import (
-	"gopkg.in/ffmt.v1"
-	"hopfield/components/binarization"
-	"hopfield/components/utils"
-	"io/ioutil"
 	"testing"
 )
 
@@ -13,29 +9,4 @@ func TestSignFunction(t *testing.T) {
 	if value >= 0 {
 		t.Fatal("unexpected value: ", value)
 	}
-}
-
-func TestCombinations(t *testing.T) {
-	dir, err := ioutil.ReadDir("../../resources/training")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	images := make([]Matrix, 0)
-	for _, filename := range dir {
-		img, err := utils.ReadImagePNG("../../resources/training/" + filename.Name())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		gray := utils.GrayscaleImage(img)
-		binRunner := binarization.CreateThresholdBinarization(gray, 128)
-		normalized := NormalizeObject(binRunner.Process())
-		matrix := ConvertToMatrix(img.Bounds().Dx(), img.Bounds().Dy(), normalized)
-
-		images = append(images, matrix)
-	}
-
-	combinations := Combinations(images...)
-	ffmt.P(combinations)
 }
---
+++ hopfield/components/hopfield/trainer_test.go
@@ -0,0 +1,59 @@
+package hopfield
+
+import (
+	"gopkg.in/ffmt.v1"
+	"hopfield/components/binarization"
+	"hopfield/components/utils"
+	"io/ioutil"
+	"testing"
+)
+
+func TestCombinations(t *testing.T) {
+	dir, err := ioutil.ReadDir("../../resources/training")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	images := make([]Matrix, 0)
+	for _, filename := range dir {
+		img, err := utils.ReadImagePNG("../../resources/training/" + filename.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		gray := utils.GrayscaleImage(img)
+		binRunner := binarization.CreateThresholdBinarization(gray, 128)
+		normalized := NormalizeObject(binRunner.Process())
+		matrix := ConvertToMatrix(img.Bounds().Dx(), img.Bounds().Dy(), normalized)
+
+		images = append(images, matrix)
+	}
+
+	combinations := Combinations(images...)
+	ffmt.P(combinations)
+}
+
+func TestGetWeights(t *testing.T) {
+	dir, err := ioutil.ReadDir("../../resources/training")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	images := make([]Matrix, 0)
+	for _, filename := range dir {
+		img, err := utils.ReadImagePNG("../../resources/training/" + filename.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		gray := utils.GrayscaleImage(img)
+		binRunner := binarization.CreateThresholdBinarization(gray, 128)
+		normalized := NormalizeObject(binRunner.Process())
+		matrix := ConvertToMatrix(img.Bounds().Dx(), img.Bounds().Dy(), normalized)
+
+		images = append(images, matrix)
+	}
+
+	w := GetWeights(images...)
+	ffmt.P(w)
+}
