024c7c85778962ead791889912d441bdc66f6b40
edelwud <edelwud@icloud.com>
2021-11-04 15:53:18 +0300

    rename: network functions

--- perceptron/internal/network/perceptron/perceptron.go
+++ perceptron/internal/network/perceptron/perceptron.go
@@ -2,24 +2,35 @@ package perceptron
 
 import (
 	"gonum.org/v1/gonum/mat"
+	"perceptron/internal/activation"
 	"perceptron/internal/layers"
 	"perceptron/internal/network"
 )
 
 type Perceptron struct {
-	Shapes []*mat.VecDense
+	Activation activation.Activation
+	Shapes     []*mat.VecDense
 }
 
 func (p Perceptron) Layers() []layers.Layer {
 	return nil
 }
 
-func (p Perceptron) Train() *mat.Dense {
-	return nil
+func (p Perceptron) BackPropagation(expect float64) {
+	return
 }
 
-func NewPerceptron(shapes ...*mat.VecDense) network.Network {
+func (p Perceptron) ForwardFeed() float64 {
+	return 0
+}
+
+func (p Perceptron) WeightsUpdater(lr float64) {
+	return
+}
+
+func NewPerceptron(activation activation.Activation, shapes ...*mat.VecDense) network.Network {
 	return &Perceptron{
-		Shapes: shapes,
+		Activation: activation,
+		Shapes:     shapes,
 	}
 }
